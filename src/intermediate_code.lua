65be773b225f45dbc44189407c1eff0894e9690a
Otávio Junqueira <ojunqueira@me.com>
2014-05-23 21:13:27 -0300

    ICG improvement
    # documentation
    # while nodes
    # if nodes
    # expression nodes
    # attribution nodes
    # return nodes
    # block nodes
    # dump function

--- src/intermediate_code.lua
+++ src/intermediate_code.lua
@@ -17,7 +17,7 @@ local NodesClass = require "lib/node_codes"
 -- Data Structure
 --==============================================================================
 
-local IntermediateCodeGen = {}
+local InterCodeGen = {}
 
 --  store file path
 local file
@@ -28,6 +28,8 @@ local label_counter = 0
 --  count number of generated variables
 local var_counter = 0
 
+local function_counter = 0
+
 --  list of nodes code
 --  {
 --    [name] = $number,
@@ -35,18 +37,34 @@ local var_counter = 0
 local nodes_codes = NodesClass.GetNodesList()
 
 -- avaiable operator codes of intermediate language
-local opcode = {
-  ["RETURN"]  = "RET",
-  ["ID=rval"] = "="
+local enum_opcodes = {
+  ["LABEL"]         = 01,
+  ["RETURN"]        = 02,
+  ["ID=rval"]       = 03,
+  ["ID=BYTErval"]   = 04,
+  ["ID=unoprval"]   = 05,
+  ["ID=rvalEQrval"] = 06,
+  ["IFFALSEGOTO"]   = 07,
+  ["GOTO"]          = 08,
 }
 
 --  three address codes
 --  {
---    strings   = {}
+--    strings   = {
+--      [1 to N] = {
+--        var = $string   - variable name
+--        str = $string   - literal string
+--      }
+--    }
 --    globals   = {
 --      [1 to N] = "name"
 --    }
---    functions = {}
+--    functions = {
+--      [1 to N] = {      - list of functions
+--        [1 to N] = {    - list of instructions
+--        }
+--      }
+--    }
 --  }
 local struct = {}
 
@@ -55,10 +73,21 @@ local struct = {}
 -- Private Methods
 --==============================================================================
 
+--AddInstruction:
+--  Parameters:
+--    [1] $table  - Table created by 'NewInstruction' function
+--  Return:
+function InterCodeGen.AddInstruction (inst)
+  if (_DEBUG) then print("ICG :: AddInstruction") end
+  assert (inst and type(inst) == "table")
+  table.insert(struct.functions[function_counter], inst)
+end
+
 --Clear: Set Initial Condition
 --  Parameters:
 --  Return:
-function IntermediateCodeGen.Clear ()
+function InterCodeGen.Clear ()
+  if (_DEBUG) then print("ICG :: Clear") end
   struct = {
     strings   = {},
     globals   = {},
@@ -66,104 +95,205 @@ function IntermediateCodeGen.Clear ()
   }
   var_counter = 0
   label_counter = 0
+  function_counter = 0
 end
 
 --Dump: Write struct to file
 --  Parameters:
 --    [1] $string   - complete file path
 --  Return:
-function IntermediateCodeGen.Dump (path)
+function InterCodeGen.Dump (output)
+  util.TablePrint(struct)
   if (_DEBUG) then print("ICG :: Dump") end
-  local f = io.open(path, "w")
-  if (not f) then
-    Error(string.format("output file '%s' could not be opened"), path)
-  end
-  for _, string in ipairs(struct.strings) do
-    -- WRITE STRINGS
+  for _, str_node in ipairs(struct.strings) do
+    output:write(string.format('%8s STRING  %s = "%s"\n', "", str_node.var, str_node.str))
   end
   for _, name in ipairs(struct.globals) do
-    f:write(string.format("%8s DECLARE %s\n", "", name))
+    output:write(string.format('%8s GLOBAL %s\n', "", name))
   end
-  for _, line in ipairs(struct.functions) do
-    f:write(line)
+  for _, func in ipairs(struct.functions) do
+    output:write(func.header)
+    for _, inst in ipairs(func) do
+      InterCodeGen.DumpInstruction(output, inst)
+    end
   end
-  f:close()
+end
+
+--DumpInstruction:
+--  Parameters:
+--    [1] $       - Desired output
+--    [2] $table  - Table created by 'AddInstruction' function
+--  Return:
+function InterCodeGen.DumpInstruction (output, inst)
+  if ((inst.code == enum_opcodes["LABEL"])) then
+    output:write(string.format('%14s\n', inst.label and ("LABEL: " .. inst.label) or ""))
+  elseif (inst.code == enum_opcodes["RETURN"]) then
+    output:write(string.format('%14s   RET %s\n', inst.label and ("LABEL: " .. inst.label) or "", inst.op1 or ""))
+  elseif (inst.code == enum_opcodes["ID=rval"]) then
+    output:write(string.format('%14s   %s = %s\n', inst.label and ("LABEL: " .. inst.label) or "", inst.op1, inst.op2))
+  elseif (inst.code == enum_opcodes["ID=BYTErval"]) then
+    output:write(string.format('%14s   %s = BYTE %s\n', inst.label and ("LABEL: " .. inst.label) or "", inst.op1, inst.op2))
+  elseif (inst.code == enum_opcodes["ID=unoprval"]) then
+    output:write(string.format('%14s   %s = %s %s\n', inst.label and ("LABEL: " .. inst.label) or "", inst.op1, inst.op2, inst.op3))
+  elseif (inst.code == enum_opcodes["ID=rvalEQrval"]) then
+    output:write(string.format('%14s   %s = %s == %s\n', inst.label and ("LABEL: " .. inst.label) or "", inst.op1, inst.op2, inst.op3))
+  elseif (inst.code == enum_opcodes["IFFALSEGOTO"]) then
+    output:write(string.format('%14s   IFFALSE %s GOTO %s\n', inst.label and ("LABEL: " .. inst.label) or "", inst.op1, inst.op2))
+  elseif (inst.code == enum_opcodes["GOTO"]) then
+    output:write(string.format('%14s   GOTO %s\n', inst.label and ("LABEL: " .. inst.label) or "", inst.op1))
+  end
 end
 
 --Error:
 --  Parameters:
+--    [1] $string - 
 --  Return:
-function IntermediateCodeGen.Error (msg)
+function InterCodeGen.Error (msg)
   local str = string.format("intermediate code generator error: %s", msg or "")
   error(str, 0)
 end
 
 --GenAttribution:
 --  Parameters:
+--    [1] $table  - 
 --  Return:
-function IntermediateCodeGen.GenAttribution (node)
+function InterCodeGen.GenAttribution (node)
   if (_DEBUG) then print("ICG :: GenAttribution") end
   assert(node.id == nodes_codes["ATTRIBUTION"])
-  local label = IntermediateCodeGen.GetLabel()
-  -- COMPLETE write expression
-  -- WHICH OP CODE?
-  --local str = IntermediateCodeGen.NewInstruction(nil, )
-  --table.insert(struct.functions, str)
+  if ((node.var.sem_type == "char" or node.var.sem_type == "bool") and (node.var.sem_dimension == 0)) then
+    InterCodeGen.AddInstruction(InterCodeGen.NewInstruction(nil, "ID=BYTErval", node.var.name, InterCodeGen.GenExpression(node.exp)))
+  else
+    InterCodeGen.AddInstruction(InterCodeGen.NewInstruction(nil, "ID=rval", node.var.name, InterCodeGen.GenExpression(node.exp)))
+  end
 end
 
 --GenBlock:
 --  Parameters:
+--    [1] $table  - 
 --  Return:
-function IntermediateCodeGen.GenBlock (block)
+function InterCodeGen.GenBlock (block)
   if (_DEBUG) then print("ICG :: GenBlock") end
   for _, node in ipairs(block) do
     if (node.id == nodes_codes["ATTRIBUTION"]) then
-      IntermediateCodeGen.GenAttribution(node)
+      InterCodeGen.GenAttribution(node)
     elseif (node.id == nodes_codes["CALL"]) then
-      -- COMPLETE
+      InterCodeGen.GenCall(node)
     elseif (node.id == nodes_codes["DECLARE"]) then
-      -- COMPLETE
+      InterCodeGen.GenDeclare(node)
     elseif (node.id == nodes_codes["IF"]) then
-      -- COMPLETE
+      InterCodeGen.GenIf(node)
     elseif (node.id == nodes_codes["RETURN"]) then
-      IntermediateCodeGen.GenReturn(node)
+      InterCodeGen.GenReturn(node)
     elseif (node.id == nodes_codes["WHILE"]) then
-      -- COMPLETE
+      InterCodeGen.GenWhile(node)
     end
   end
 end
 
---GenExpression:
+--GenCall: 
 --  Parameters:
+--    [1] $table  - 
 --  Return:
-function IntermediateCodeGen.GenExpression (var, node)
-  local str = "expression here return at " .. var .. "\n"
-  print(node.id)
-  if (node.id == nodes_codes["CALL"]) then
-    
-  elseif (node.id == nodes_codes["NEGATE"]) then
-    
-  elseif (node.id == nodes_codes["NEWVAR"]) then
-    
-  elseif (node.id == nodes_codes["OPERATOR"]) then
-    
-  elseif (node.id == nodes_codes["UNARY"]) then
-    
-  elseif (node.id == nodes_codes["VALUE"]) then
-    local str = IntermediateCodeGen.NewInstruction(nil, "ID=rval", var, node.value)
-    table.insert(struct.functions, str)
-  elseif (node.id == nodes_codes["VAR"]) then
-    
-  end
-  
+function InterCodeGen.GenCall (node)
+  if (_DEBUG) then print("ICG :: GenCall") end
+  assert(node.id == nodes_codes["CALL"])
+  -- COMPLETE
 end
 
---GenFunction:
+--GenDeclare: Create a new instruction and add it to current function
 --  Parameters:
+--    [1] $table  - 
 --  Return:
-function IntermediateCodeGen.GenFunction (node)
+function InterCodeGen.GenDeclare (node)
+  if (_DEBUG) then print("ICG :: GenDeclare") end
+  assert(node.id == nodes_codes["DECLARE"])
+  local op
+  if ((node.type == "bool" or node.type == "char") and node.dimension == 0) then
+    op = "ID=BYTErval"
+  else
+    op = "ID=rval"
+  end
+  InterCodeGen.AddInstruction(InterCodeGen.NewInstruction(nil, op, node.name, "0"))
+end
+
+--GenExpression:
+--  Parameters:
+--    [1] $table  - 
+--  Return:
+--    [1] $string - Variable or value of expression return
+function InterCodeGen.GenExpression (node)
+  if (node.id == nodes_codes["CALL"]) then
+    return InterCodeGen.GenExpressionCall(node)
+  elseif (node.id == nodes_codes["LITERAL"]) then
+    return InterCodeGen.GenExpressionLiteral(node)
+  elseif (node.id == nodes_codes["NEGATE"]) then
+    return InterCodeGen.GenExpressionNegate(node)
+  elseif (node.id == nodes_codes["NEWVAR"]) then
+    return InterCodeGen.GenExpressionNewVar(node)
+  elseif (node.id == nodes_codes["OPERATOR"]) then
+    return InterCodeGen.GenExpressionOperator(node)
+  elseif (node.id == nodes_codes["UNARY"]) then
+    return InterCodeGen.GenExpressionUnary(node)
+  elseif (node.id == nodes_codes["VAR"]) then
+    return InterCodeGen.GenExpressionVar(node)
+  end
+end
+
+function InterCodeGen.GenExpressionCall (node)
+  -- COMPLETE
+end
+
+function InterCodeGen.GenExpressionLiteral (node)
+  local op
+  if (node.type == "char") then
+    op = InterCodeGen.GetVariable()
+    local t = {
+      var = op,
+      str = node.value,
+    }
+    table.insert(struct.strings, t)
+  elseif (node.type == "bool") then
+    op = (node.value == "true" and "1") or "0"
+  else
+    op = node.value
+  end
+  return op
+end
+
+function InterCodeGen.GenExpressionNegate (node)
+  local op = InterCodeGen.GetVariable()
+  InterCodeGen.AddInstruction(InterCodeGen.NewInstruction(nil, "ID=rvalEQrval", op, InterCodeGen.GenExpression(node.exp), "0"))
+  return op
+end
+
+function InterCodeGen.GenExpressionNewVar (node)
+  -- COMPLETE
+end
+
+function InterCodeGen.GenExpressionOperator (node)
+  --[and or + - * / > < >= <= = <>]
+  -- COMPLETE
+end
+
+function InterCodeGen.GenExpressionUnary (node)
+  local op = InterCodeGen.GetVariable()
+  InterCodeGen.AddInstruction(InterCodeGen.NewInstruction(nil, "ID=unoprval", op, "-", InterCodeGen.GenExpression(node.exp)))
+  return op
+end
+
+function InterCodeGen.GenExpressionVar (node)
+  -- COMPLETE
+  -- verify dimension
+end
+
+--GenFunction: 
+--  Parameters:
+--    [1] $table  - 
+--  Return:
+function InterCodeGen.GenFunction (node)
   if (_DEBUG) then print("ICG :: GenFunction") end
   assert(node.id == nodes_codes["FUNCTION"])
+  function_counter = function_counter + 1
   local header = string.format("%8s FUN %s (", "", node.name)
   if (node.params and node.params[1]) then
     header = header .. node.params[1].name
@@ -174,57 +304,122 @@ function IntermediateCodeGen.GenFunction (node)
     end
   end
   header = header .. ")\n"
-  table.insert(struct.functions, header)
-  IntermediateCodeGen.GenBlock(node.block)
+  struct.functions[function_counter] = {
+    header = header,
+  }
+  InterCodeGen.GenBlock(node.block)
 end
 
 --GenGlobal:
 --  Parameters:
+--    [1] $table  - 
 --  Return:
-function IntermediateCodeGen.GenGlobal (node)
+function InterCodeGen.GenGlobal (node)
   if (_DEBUG) then print("ICG :: GenGlobal") end
   assert(node.id == nodes_codes["DECLARE"])
   table.insert(struct.globals, node.name)
 end
 
---GenLiteralString:
+--GenIf:
 --  Parameters:
+--    [1] $table  - 
 --  Return:
-function IntermediateCodeGen.GenLiteralString (node)
-  if (_DEBUG) then print("ICG :: GenLiteralString") end
-  -- COMPLETE
+function InterCodeGen.GenIf(node)
+  if (_DEBUG) then print("ICG :: GenIf") end
+  assert(node.id == nodes_codes["IF"])
+  local lbl_end       = InterCodeGen.GetLabel()
+  local var_condition = InterCodeGen.GetVariable()
+  local op = InterCodeGen.GenExpression(node.cond)
+  InterCodeGen.AddInstruction(InterCodeGen.NewInstruction(nil, "ID=BYTErval", var_condition, op))
+  if (node["elseif"] or node["else"]) then
+    local lbl_next = InterCodeGen.GetLabel()
+    InterCodeGen.AddInstruction(InterCodeGen.NewInstruction(nil, "IFFALSEGOTO", var_condition, lbl_next))
+    InterCodeGen.GenBlock(node.block)
+    InterCodeGen.AddInstruction(InterCodeGen.NewInstruction(nil, "GOTO", lbl_end))
+    if (node["elseif"]) then
+      for i = 1, #node["elseif"] do
+        InterCodeGen.AddInstruction(InterCodeGen.NewInstruction(lbl_next, "LABEL"))
+        var_condition = InterCodeGen.GetVariable()
+        op = InterCodeGen.GenExpression(node["elseif"][i].cond)
+        InterCodeGen.AddInstruction(InterCodeGen.NewInstruction(nil, "ID=BYTErval", var_condition, op))
+        if ((#node["elseif"] - i) > 0) then
+          lbl_next = InterCodeGen.GetLabel()
+          InterCodeGen.AddInstruction(InterCodeGen.NewInstruction(nil, "IFFALSEGOTO", var_condition, lbl_next))
+          InterCodeGen.GenBlock(node["elseif"][i].block)
+          InterCodeGen.AddInstruction(InterCodeGen.NewInstruction(nil, "GOTO", lbl_end))
+        else
+          if (node["else"]) then
+            lbl_next = InterCodeGen.GetLabel()
+            InterCodeGen.AddInstruction(InterCodeGen.NewInstruction(nil, "IFFALSEGOTO", var_condition, lbl_next))
+            InterCodeGen.GenBlock(node["elseif"][i].block)
+            InterCodeGen.AddInstruction(InterCodeGen.NewInstruction(nil, "GOTO", lbl_end))
+          else
+            InterCodeGen.AddInstruction(InterCodeGen.NewInstruction(nil, "IFFALSEGOTO", var_condition, lbl_end))
+            InterCodeGen.GenBlock(node["elseif"][i].block)
+          end
+        end
+      end
+    end
+    if (node["else"]) then
+      InterCodeGen.AddInstruction(InterCodeGen.NewInstruction(lbl_next, "LABEL"))
+      InterCodeGen.GenBlock(node["else"])
+    end
+  else
+    InterCodeGen.AddInstruction(InterCodeGen.NewInstruction(nil, "IFFALSEGOTO", var_condition, lbl_end))
+    InterCodeGen.GenBlock(node.block)
+  end
+  InterCodeGen.AddInstruction(InterCodeGen.NewInstruction(lbl_end, "LABEL"))
 end
 
 --GenReturn:
 --  Parameters:
+--    [1] $table  - 
 --  Return:
-function IntermediateCodeGen.GenReturn (node)
+function InterCodeGen.GenReturn (node)
   if (_DEBUG) then print("ICG :: GenReturn") end
   assert(node.id == nodes_codes["RETURN"])
-  local str, var
+  local op
   if (node.exp) then
-    var = IntermediateCodeGen.GetVariable()
-    IntermediateCodeGen.GenExpression(var, node.exp)
+    op = InterCodeGen.GenExpression(node.exp)
   end
-  str = IntermediateCodeGen.NewInstruction(nil, "RETURN", var)
-  table.insert(struct.functions, str)
+  local t = InterCodeGen.NewInstruction(nil, "RETURN", op)
+  InterCodeGen.AddInstruction(t)
+end
+
+--GenWhile:
+--  Parameters:
+--    [1] $table  - 
+--  Return:
+function InterCodeGen.GenWhile (node)
+  if (_DEBUG) then print("ICG :: GenWhile") end
+  assert(node.id == nodes_codes["WHILE"])
+  local lbl_before    = InterCodeGen.GetLabel()
+  local lbl_after     = InterCodeGen.GetLabel()
+  local var_condition = InterCodeGen.GetVariable()
+  InterCodeGen.AddInstruction(InterCodeGen.NewInstruction(lbl_before, "LABEL"))
+  local op = InterCodeGen.GenExpression(node.cond)
+  InterCodeGen.AddInstruction(InterCodeGen.NewInstruction(nil, "ID=BYTErval", var_condition, op))
+  InterCodeGen.AddInstruction(InterCodeGen.NewInstruction(nil, "IFFALSEGOTO", var_condition, lbl_after))
+  InterCodeGen.GenBlock(node.block)
+  InterCodeGen.AddInstruction(InterCodeGen.NewInstruction(nil, "GOTO", lbl_before))
+  InterCodeGen.AddInstruction(InterCodeGen.NewInstruction(lbl_after, "LABEL"))
 end
 
 --GetLabel: Get a new string to use as a label
 --  Parameters:
 --  Return:
 --    [1] $string   - New unique label
-function IntermediateCodeGen.GetLabel ()
+function InterCodeGen.GetLabel ()
   if (_DEBUG) then print("ICG :: GetLabel") end
   label_counter = label_counter + 1
-  return "$L" .. label_counter
+  return ".L" .. label_counter
 end
 
 --GetVariable: Get a new string to use as a variable
 --  Parameters:
 --  Return:
---    [1] $string   - New unique label
-function IntermediateCodeGen.GetVariable ()
+--    [1] $string   - New unique variable
+function InterCodeGen.GetVariable ()
   if (_DEBUG) then print("ICG :: GetVariable") end
   var_counter = var_counter + 1
   return "$t" .. var_counter
@@ -232,17 +427,23 @@ end
 
 --NewInstruction:
 --  Parameters:
+--    [1] $string - 
+--    [2] $string - 
+--    [3] $string - 
+--    [4] $string - 
+--    [5] $string - 
 --  Return:
-function IntermediateCodeGen.NewInstruction (label, code, op1, op2, op3)
+function InterCodeGen.NewInstruction (label, code, operator1, operator2, operator3)
   if (_DEBUG) then print("ICG :: NewInstruction") end
-  assert (opcode) -- opcode[opcode]
-  local str
-  if (code == "RETURN") then
-    str = string.format("%14s   %s %s\n", label and ("LABEL: " .. label) or "", opcode[code], op1 or "")
-  elseif (code == "ID=rval") then
-    str = string.format("%14s   %s %s %s\n", label and ("LABEL: " .. label) or "", op1, opcode[code], op2)
-  end
-  return str
+  assert (enum_opcodes[code])
+  local t = {
+    label = label,
+    code  = enum_opcodes[code],
+    op1   = operator1,
+    op2   = operator2,
+    op3   = operator3,
+  }
+  return t
 end
 
 
@@ -257,22 +458,27 @@ end
 --  return:
 --    [1] $boolean  - false if found any problem, true otherwise
 --    [2] $string   - only when [1] is false, informing which error occurs
-function IntermediateCodeGen.Open (path, tree)
+function InterCodeGen.Open (path, tree)
   if (_DEBUG) then print("ICG :: Open") end
   assert(path)
   assert(tree and type(tree) == "table")
-  IntermediateCodeGen.Clear()
+  InterCodeGen.Clear()
   local ok, msg = pcall(function ()
     for _, node in ipairs(tree) do
       if (node.id == nodes_codes["DECLARE"]) then
-        IntermediateCodeGen.GenGlobal(node)
+        InterCodeGen.GenGlobal(node)
       elseif (node.id == nodes_codes["FUNCTION"]) then
-        IntermediateCodeGen.GenFunction(node)
+        InterCodeGen.GenFunction(node)
       else
-        IntermediateCodeGen.Error("unknown program node.")
+        InterCodeGen.Error("unknown program node.")
       end
     end
-    IntermediateCodeGen.Dump(util.FileRemoveExtension(path) .. ".icg")
+    local f = io.open(util.FileRemoveExtension(path) .. ".icg", "w")
+    if (not f) then
+      Error(string.format("output file '%s' could not be opened"), path)
+    end
+    InterCodeGen.Dump(f)
+    f:close()
     if (printStruct) then
       util.TablePrint(struct)
     end
@@ -288,7 +494,7 @@ end
 -- Return
 --==============================================================================
 
-return IntermediateCodeGen
+return InterCodeGen
 
 
 --[[
